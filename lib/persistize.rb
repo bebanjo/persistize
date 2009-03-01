899544902bb24c8f7d558bc0ba87a2014d4d00c0
Luismi Cavalle <luismi@lmcavalle.com>
2009-03-01 02:11:56 +0100

    Changes for gem building on github

--- lib/persistize.rb
+++ lib/persistize.rb
@@ -1,50 +1,51 @@
-class ActiveRecord::Base
-
-  def self.persistize(*args)
+module Persistize
+  module ClassMethods
+    def persistize(*args)
+      options = args.pop if args.last.is_a?(Hash)
     
-    options = args.pop if args.last.is_a?(Hash)
-    
-    args.each do |method|
+      args.each do |method|
       
-      attribute = method.to_s.sub(/\?$/, '')
+        attribute = method.to_s.sub(/\?$/, '')
       
-      alias_method "#{attribute}_calculation", method
+        alias_method "#{attribute}_calculation", method
       
-      define_method(method) do
-        if new_record?
-          send("#{attribute}_calculation")
-        else
-          self[attribute]
+        define_method(method) do
+          if new_record?
+            send("#{attribute}_calculation")
+          else
+            self[attribute]
+          end
         end
-      end
 
-      before_save "update_#{attribute}"
+        before_save "update_#{attribute}"
       
-      define_method("update_#{attribute}") do
-        self[attribute] = send("#{attribute}_calculation")
-        true # return true to avoid canceling the save
-      end
-
-      if options && options[:depending_on]
-        dependencies = options[:depending_on].is_a?(Array) ? options[:depending_on] : [options[:depending_on]]
-        dependencies.each do |dependency|
-          association = self.reflections[dependency]
-          me = self
-          callback = "update_#{attribute}_in_#{me.to_s.underscore}"
-          association.klass.class_eval do
-            define_method(callback) do
-              return true unless parent_id = self[association.primary_key_name]
-              parent = me.find(parent_id)
-              parent.send("update_#{attribute}")
-              parent.save!            
-            end
-            after_save callback
-            after_destroy callback
-          end          
+        define_method("update_#{attribute}") do
+          self[attribute] = send("#{attribute}_calculation")
+          true # return true to avoid canceling the save
         end
-      end
+
+        if options && options[:depending_on]
+          dependencies = options[:depending_on].is_a?(Array) ? options[:depending_on] : [options[:depending_on]]
+          dependencies.each do |dependency|
+            association = self.reflections[dependency]
+            me = self
+            callback = "update_#{attribute}_in_#{me.to_s.underscore}"
+            association.klass.class_eval do
+              define_method(callback) do
+                return true unless parent_id = self[association.primary_key_name]
+                parent = me.find(parent_id)
+                parent.send("update_#{attribute}")
+                parent.save!            
+              end
+              after_save callback
+              after_destroy callback
+            end          
+          end
+        end
       
+      end
     end
   end
-
 end
+
+ActiveRecord::Base.extend(Persistize::ClassMethods)
---
+++ persistize.gemspec
@@ -0,0 +1,18 @@
+Gem::Specification.new do |s|
+  s.name = "persistize"
+  s.version = "0.0.1"
+  s.authors = ["Sergio Gil", "Luismi Cavallé"]
+  s.email = "sgilperez@gmail.com"
+  s.homepage = "http://github.com/porras/persistize"
+  s.summary = "Rails plugin for easy denormalization"
+  s.has_rdoc = true
+  s.extra_rdoc_files = ["README.rdoc"]
+  s.rdoc_options = ["--line-numbers", "--inline-source", "--main", "README.rdoc"]
+  s.files = [
+    "README.rdoc",
+    "lib/persistize.rb"
+  ]
+  
+  # s.rubyforge_project = "persistize"
+  
+end
